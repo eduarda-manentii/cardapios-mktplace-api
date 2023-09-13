30026dd020d882274d8f7d62b5612632657381fb
Eduarda Manenti <eduarda_manenti@nuntec.com.br>
2023-09-13 17:23:03 -0300

    Corrigido os métodos dentro do service do cardápio.

--- src/main/java/br/com/senai/cardapiosmktplaceapi/service/CardapioService.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/service/CardapioService.java
@@ -2,46 +2,44 @@ package br.com.senai.cardapiosmktplaceapi.service;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
 
 import br.com.senai.cardapiosmktplaceapi.dto.CardapioSalvo;
-import br.com.senai.cardapiosmktplaceapi.entity.Categoria;
+import br.com.senai.cardapiosmktplaceapi.dto.NovoCardapio;
+import br.com.senai.cardapiosmktplaceapi.entity.Cardapio;
 import br.com.senai.cardapiosmktplaceapi.entity.Restaurante;
 import br.com.senai.cardapiosmktplaceapi.entity.enums.Status;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+@Validated
 public interface CardapioService  {
 	
-	public Categoria inserir(
-			@NotNull(message = "A categoria é obrigatória.") 
-			Categoria categoria);
-	
-	public void alterar(
+	public Cardapio inserir(
 			@Valid
-			@NotNull(message = "O cardápio salvo é obrigatório.")
-			CardapioSalvo cardapioSavlo);
+			@NotNull(message = "O novo cardápio é obrigatório")
+			NovoCardapio novoCardapio);
 	
-	public Page<Categoria> listarPor(
-			@NotNull(message = "O restaurante é obrigatório.")
-			Restaurante restaurante,
-			Pageable paginacao);
+	public Cardapio alterar(
+			@Valid
+			@NotNull(message = "O cardápio salvo é obrigatório")
+			CardapioSalvo cardapioSalvo);
 	
-	public Categoria buscarPor(
-			@NotNull(message = "O id é obrigatório.") 
-			@Positive(message = "O id deve ser positivo.")
+	public Page<Cardapio> listarPor(
+			@NotNull(message = "O restaurante é obrigatório")
+			Restaurante restaurante, Pageable paginacao);
+	
+	public Cardapio buscarPor(
+			@NotNull(message = "O id é obrigatório")
+			@Positive(message = "O id deve ser positivo")
 			Integer id);
 	
 	public void atualizarStatusPor(
-			@NotNull(message = "O id é obrigatório.") 
-			@Positive(message = "O id deve ser positivo.")
-			Integer id, 
-			@NotNull(message = "O status é obrigatório.")
-			Status status);
-	
-	public Categoria excluirPor(
-			@NotNull(message = "O id para a exclusão é obrigatório.") 
-			@Positive(message = "O id para a exclusão deve ser positivo.")
-			Integer id);
+			@NotNull(message = "O id é obrigatório")
+			@Positive(message = "O id deve ser positivo")
+			Integer id,
+			@NotNull(message = "O status é obrigatório")
+			Status status);	
 
 }
