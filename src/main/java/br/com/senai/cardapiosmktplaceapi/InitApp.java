7702ce58e9e7a2ba8551d75d6ea054f8f1218225
Eduarda Manenti <dudamanenti65@gmail.com>
2023-09-20 19:25:48 -0300

    Ajustado o initApp.

--- src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
@@ -1,15 +1,9 @@
 package br.com.senai.cardapiosmktplaceapi;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import br.com.senai.cardapiosmktplaceapi.entity.Opcao;
-import br.com.senai.cardapiosmktplaceapi.service.impl.CategoriaServiceImpl;
-import br.com.senai.cardapiosmktplaceapi.service.impl.OpcaoServiceImpl;
-import br.com.senai.cardapiosmktplaceapi.service.impl.RestauranteServiceImpl;
 
 @SpringBootApplication
 public class InitApp {
@@ -18,23 +12,9 @@ public class InitApp {
         SpringApplication.run(InitApp.class, args);
     }
     
-    @Autowired
-    private RestauranteServiceImpl restauranteService;
-    
-    @Autowired
-    private CategoriaServiceImpl categoriaService;
-    
-    @Autowired
-    private OpcaoServiceImpl opcaoService;
-    
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-        	
-        	 // Exemplo de teste para buscar uma opção por código
-            Integer codigoOpcao = 205;
-            Opcao opcaoEncontrada = opcaoService.buscarPor(codigoOpcao);
-            System.out.println("Opção encontrada: " + opcaoEncontrada);
 
         };
         
