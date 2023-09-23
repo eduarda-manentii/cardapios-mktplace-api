1183e903a0015f3f8a334ab943af8456247f8921
Eduarda Manenti <dudamanenti65@gmail.com>
2023-09-22 22:16:07 -0300

    Inicio da implementação de segurança da API.

---
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/repository/UsuariosRepository.java
@@ -0,0 +1,25 @@
+package br.com.senai.cardapiosmktplaceapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import br.com.senai.cardapiosmktplaceapi.entity.Usuario;
+import jakarta.transaction.Transactional;
+
+@Repository
+public interface UsuariosRepository extends JpaRepository<Usuario, Integer> {
+
+	@Query(value = "SELECT u FROM Usuario u WHERE u.login = :login")
+	public Usuario buscarPor(String login);
+	
+	@Modifying @Transactional
+	@Query(value = "UPDATE Usuario u SET u.nome = :nome, u.senha = :senha WHERE u.login = :login")
+	public void atualizarPor( String login, String nome, String senha);
+
+	@Modifying @Transactional
+	@Query(value = "UPDATE Usuario u SET u.senha = :senha WHERE u.login = :login")
+	public void atualizarPor(String login,String senha);
+	
+}
---
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/security/FiltrosDeAutenticacaoJWT.java
@@ -0,0 +1,57 @@
+package br.com.senai.cardapiosmktplaceapi.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.senai.cardapiosmktplaceapi.service.impl.CredencialDeAcessoServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FiltrosDeAutenticacaoJWT extends OncePerRequestFilter {
+	
+	@Autowired
+	private GerenciadorDeTokenJWT gerenciadorDeToken;
+
+	@Autowired
+	private CredencialDeAcessoServiceImpl service;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request,
+			HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		String authHeader = request.getHeader("Authorization");
+		String token = null;
+		String login = null;
+		boolean isHeaderAuthorizationPresent = authHeader != null &&
+				authHeader.startsWith("Bearer ");
+		if (isHeaderAuthorizationPresent) {
+			token = authHeader.substring(7);
+			login = gerenciadorDeToken.extrairLoginDo(token);
+		}
+		
+		boolean isNovoLogin = login != null 
+				&& SecurityContextHolder.getContext().getAuthentication() == null;
+		if (isNovoLogin) {
+			UserDetails credencial = service.loadUserByUsername(login);
+			if (gerenciadorDeToken.isValido(token, credencial)) {
+				UsernamePasswordAuthenticationToken tokenAutenticado = 
+						new UsernamePasswordAuthenticationToken(
+								credencial, null, credencial.getAuthorities());
+				tokenAutenticado.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(tokenAutenticado);
+			}
+		}
+		filterChain.doFilter(request, response);
+	}
+
+}
